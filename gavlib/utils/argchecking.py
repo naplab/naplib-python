71b885cffb7c81d2b33e8c9737f33bca42ae967e
Gavin Mischler <gavinmischler@gmail.com>
2022-02-16 15:47:18 -0500

    add docstring to parse args

--- gavlib/utils/argchecking.py
+++ gavlib/utils/argchecking.py
@@ -1,6 +1,25 @@
 from ..out_struct import OutStruct
 
 def _parse_outstruct_args(outstruct, *args):
+    '''
+    Parse a variable number of arguments and return a tuple with either the field
+    from the outstruct which is specified by a given arg or the arg itself if it
+    does not correspond to a field in the outstruct.
+    
+    Parameters
+    ----------
+    outstruct : any type or None
+        If a naplib.OutStruct object, then will check if the other args are fields
+        of this object, otherwise they will be returned without change.
+    args : variable number of inputs
+        Other arguments. Each one will be checked to see if it is a field of the
+        outstruct, and if it is then it will be replaced by that field, otherwise
+        it will be returned without change.
+
+    Returns
+    -------
+    out : single arg or tuple of args, same length as *args input
+    '''
     if not isinstance(outstruct, OutStruct):
         if len(args) > 1:
             return args
