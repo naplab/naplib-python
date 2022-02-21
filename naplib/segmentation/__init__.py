2af1e70002466e99174cd8e75b44476e324dd36f
Gavin Mischler <gavinmischler@gmail.com>
2022-02-21 14:23:39 -0500

    change electrode lags name to include fratio

--- naplib/segmentation/__init__.py
+++ naplib/segmentation/__init__.py
@@ -1,5 +1,5 @@
 from .segmentation import get_label_change_points
 from .segmentation import segment_around_labeltransitions
-from .segmentation import compute_electrode_lags
+from .segmentation import electrode_lags_fratio
 
-__all__  = ['get_label_change_points','segment_around_labeltransitions','compute_electrode_lags']
+__all__  = ['get_label_change_points','segment_around_labeltransitions','electrode_lags_fratio']
