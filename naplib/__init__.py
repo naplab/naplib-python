b331bdb901fd81dd9dfbffd33552c2f9584732b5
Gavin Mischler <gavinmischler@gmail.com>
2022-02-22 23:44:29 -0500

    add missing preprocessing import in init

--- naplib/__init__.py
+++ naplib/__init__.py
@@ -3,6 +3,7 @@ import naplib.segmentation
 import naplib.stats
 import naplib.visualization
 import naplib.io
+import naplib.preprocessing
 import naplib.utils
 from .out_struct import OutStruct, join_fields
 
