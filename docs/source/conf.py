9d6c083232a6636223715af08ebd7774dbc39d97
Gavin Mischler <gavinmischler@gmail.com>
2022-04-29 10:53:54 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,6 +56,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['build', '**.ipynb_checkpoints']
 
+master_doc = 'index'
 
 # -- Options for HTML output -------------------------------------------------
 
