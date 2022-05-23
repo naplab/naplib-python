c8db45c1158d0763fdb29890375ce1dab5e73834
Gavin Mischler <gavinmischler@gmail.com>
2022-05-23 17:02:56 -0400

    update segmentation

--- naplib/alignment/aligner.py
+++ naplib/alignment/aligner.py
@@ -4,6 +4,8 @@ import sys
 import unicodedata
 import string
 
+from ..utils import _parse_outstruct_args
+
 
 class Aligner():
     '''
@@ -26,6 +28,8 @@ class Aligner():
     
     Parameters
     ----------
+    output_dir : string, path-like
+            Directory to put output files in, such as .phn, .wrd., and .TextGrid files.
     dictionary_file : string, path-like, optional
         Path to a dictionary file (e.g. eng.dict) which contains phonemes for
         all words in corpus. If not provided, will use the default eng.dict.
@@ -35,8 +39,9 @@ class Aligner():
         Directory to hold temporary files that are created. If not provided, creates
         a folder called `data_/` in the current working directory and uses that.
     '''
-    def __init__(self, dictionary_file=None, tmp_dir=None):
+    def __init__(self, output_dir, dictionary_file=None, tmp_dir=None):
 
+        os.makedirs(output_dir, exist_ok=True)
         if tmp_dir is None:
             tmp_dir = 'data_/'
             # check if this folder already exists, in which case throw an error
@@ -46,12 +51,12 @@ class Aligner():
                     'the default "data_/" because a folder with that name '
                     'already exists in the current path. Please remove that '
                     'directory or explicitly specify the tmp_dir parameter.')
+        self.output_dir = output_dir
         self.tmp_dir = tmp_dir
         self.filedir_ = dirname(__file__)
         if dictionary_file is None:
             dictionary_file = join(self.filedir_, 'eng.dict')
         self.dictionary_file = dictionary_file
-        
 
         try:
             import yaml
@@ -84,7 +89,7 @@ class Aligner():
         ascii_file = open(os.path.join(new_folder, new_name), 'wb')
         ascii_file.write(ascii_data)
 
-    def align_from_files(self, audio_dir, text_dir, output_dir):
+    def align_from_files(self, audio_dir, text_dir):
         '''
         Perform alignment across a set of paired audio-text files stored
         in directories. This function will create a set of .TextGrid files,
@@ -102,8 +107,6 @@ class Aligner():
         text_dir : string, path-like
             Directory containing text files (.txt) with matching names
             to the files in ``audio_dir``.
-        output_dir : string, path-like
-            Directory to put output files in.
 
         Note
         ----
@@ -137,6 +140,147 @@ class Aligner():
         | │   └── file2.phn
         | │   └── file2.wrd
         | │   └── file2.TextGrid
+        '''
+        import textgrid
+
+        print(f'Resampling audio and putting in {self.tmp_dir} directory...')
+
+        resample_path = join(self.filedir_, 'resample.sh')
+
+        # resample the audios to 16000 and put them in the tmp data folder
+        os.system(f'{resample_path} -s 16000 -r {audio_dir} -w {self.tmp_dir}')
+
+        print(f'Converting text files to ascii in {self.tmp_dir} directory...')
+
+        for root, dirs, files in os.walk(text_dir, topdown=False):
+            for name in files:
+                if '.txt' in name:
+                    self._convert_text_to_ascii(name, root)
+
+        print('Performing alignment...')
+
+        # perform alignment using ProsodyLab-Aligner
+        sys.path.insert(1, self.filedir_)
+        prosodylab_main_file = join(self.filedir_, 'prosodylab_aligner/__main__.py')
+        eng_zip_file = join(self.filedir_, 'eng.zip')
+        os.system(f'python3 {prosodylab_main_file} -a {self.tmp_dir} -d {self.dictionary_file} -r {eng_zip_file}')
+        sys.path.remove(self.filedir_)
+
+        print(f'Converting .TextGrid files to .phn and .wrd in {self.output_dir}')
+
+        # Convert textgrid files to .phn and .wrd files in output_dir
+        for root, dirs, files in os.walk(self.tmp_dir, topdown=False):
+            for name in files:
+                if '.TextGrid' in name:
+
+                    # print(f'looking at {os.path.join(root, name)}')
+
+                    # copy TextGrid file to output_dir so they are saved
+                    os.system(f'cp {join(root, name)} {join(self.output_dir, name)}')
+
+                    new_phn_name = name.replace('.TextGrid', '.phn')
+                    new_wrd_name = name.replace('.TextGrid', '.wrd')
+
+                    tg = textgrid.TextGrid.fromFile(join(root, name))
+                    phones = tg[0]
+                    words = tg[1]
+
+                    # write phn file
+
+                    phn_file = open(os.path.join(self.output_dir, new_phn_name), 'w')
+
+                    for phone_seg in phones:
+                        if phone_seg.mark == "":
+                            phone_seg.mark = "sp"
+                        if phone_seg.mark != "sil":
+                            print(f"{phone_seg.minTime} {phone_seg.maxTime} {phone_seg.mark}", file=phn_file)
+
+                    phn_file.close()
+
+                    # write wrd file
+
+                    wrd_file = open(os.path.join(self.output_dir, new_wrd_name), 'w')
+
+                    for word_seg in words:
+                        if word_seg.mark != "sil":
+                            print(f"{word_seg.minTime} {word_seg.maxTime} {word_seg.mark}", file=wrd_file)
+
+                    wrd_file.close()
+
+
+        print('All done!')
+
+    def align(self, outstruct=None, name='name', sound='sound',
+              soundf='soundf', transcript='transcript',
+              dataf='dataf'):
+        '''
+        Perform alignment across a set of paired audio-text files stored
+        in fields of an OutStruct. This function will create a set of
+        .TextGrid files, as well as corresponding .phn and .wrd
+        files in the output_dir which describe the timing of phonemes and
+        words within each audio. These files can be used in conjunction
+        with the other functions in `naplib.alignment`, such as
+        ``get_phoneme_label_vector`` and ``get_word_label_vector``,
+        which take these files as input. This function will automatically
+        use ``naplib.alignment.get_phoneme_label_vector`` and
+        ``naplib.alignment.get_word_label_vector`` to produce phoneme and
+        word label vectors for each stimulus which can be placed into the
+        OutStruct and further analyzed.
+
+        Parameters
+        ----------
+        outstruct : naplib.OutStruct
+            OutStruct containing the data to align. It must contain the
+            following fields. 
+        name : string or list of strings, default='name'
+            If a string, specifies a field of the outstruct which contains
+            the name for each trial. Otherwise, a list of strings specifies
+            the name for each trial.
+        sound : string or list of np.ndarrays, default='sound'
+            If a string, specifies a field of the outstruct which contains
+            the sound waveform for each trial. Otherwise, a list of np.ndarrays
+            specifies the waveform for each trial.
+        soundf : string, integer, or list of integers
+            If a string, specifies a field of the outstruct which contains
+            the sampling rate for each trial. Otherwise, a list of integers
+            specifies the sampling rate for each trial, or a single integer gives the
+            sampling rate for all trials.
+        transcript : string or list of strings
+            If a string, specifies a field of the outstruct which contains
+            the transcript text for each trial. Otherwise, a list of strings
+            specifies the transcript text for each trial.
+        dataf : string, integer, or list of integers
+            If a string, specifies a field of the outstruct which contains
+            the desired sampling rate of the output. Otherwise, a list of integers
+            specifies the Desired sampling rate of the output for each trial, or
+            a single integer gives the desired sampling rate of the output
+            for all trials.
+        length : string or list of integers
+            If a string, specifies a field of the outstruct which contains
+            the desired output length (in samples) for each trial. Otherwise,
+            a list of integers specifies the desired output length (in samples)
+            for each trial.
+
+        Returns
+        -------
+        phoneme_labels : list of np.ndarrays
+            Phoneme label vector for each trial. phoneme_labels[i] is a np.ndarray
+            of shape (time,) and sampling rate 
+
+        Note
+        ----
+        After running this function, several files will be created and put into
+        the output_dir. If the `name` field of each trial is "trial1", "trial2",
+        etc, then the output_dir will look like this.
+
+        | working directory
+        | └── output_dir
+        | │   └── trial1.phn
+        | │   └── trial1.wrd
+        | │   └── trial1.TextGrid
+        | │   └── trial2.phn
+        | │   └── trial2.wrd
+        | │   └── trial2.TextGrid
         '''
         import textgrid
 
--- naplib/utils/argchecking.py
+++ naplib/utils/argchecking.py
@@ -1,6 +1,8 @@
+from itertools import groupby
+
 from ..out_struct import OutStruct
 
-def _parse_outstruct_args(outstruct, *args):
+def _parse_outstruct_args(outstruct, *args, allow_different_lengths=False):
     '''
     Parse a variable number of arguments and return a tuple with either the field
     from the outstruct which is specified by a given arg or the arg itself if it
@@ -11,21 +13,39 @@ def _parse_outstruct_args(outstruct, *args):
     outstruct : any type or None
         If a naplib.OutStruct object, then will check if the other args are fields
         of this object, otherwise they will be returned without change.
-    args : variable number of inputs
+    *args : variable number of inputs
         Other arguments. Each one will be checked to see if it is a field of the
-        outstruct, and if it is then it will be replaced by that field, otherwise
-        it will be returned without change.
+        outstruct, and if it is then it will be replaced by that field. If it is a
+        list, it will be returned without change, and if it is any other type, it will
+        be repeated into a list of the same length as the outstruct or any other list args,
+        and if nothing else is provided, it will be return as is.
+    allow_different_lengths : bool, default=False
+        If True, args are not required to all have the same length, and they do not need
+        to be the same length as the outstruct if one is provided.
 
     Returns
     -------
-    out : single arg or tuple of args, same length as *args input
+    *args_out : single arg or tuple of args, same length as *args input
     '''
+    # if not outstruct was provided, then just parse the args on their own
     if not isinstance(outstruct, OutStruct):
+        args = list(args)
         if len(args) > 1:
-            return args
+            lengths = []
+            for i, arg in enumerate(args):
+                if isinstance(arg, list):
+                    lengths.append(len(arg))
+            if len(lengths) > 0:
+                if not allow_different_lengths and not _all_equal_list(lengths):
+                    raise ValueError(f"The supplied list arguments have different lengths, but they must all be the same length.")
+                for i, arg in enumerate(args):
+                    if not isinstance(arg, list) and not isinstance(arg, tuple):
+                        args[i] = [arg for _ in range(lengths[0])]
+            return tuple(args)
         else:
             return args[0]
     
+    # if an outstruct was provided, check if args are fields of the outstruct
     args_out = []
     
     outstruct_fieldnames = outstruct.fields
@@ -33,8 +53,20 @@ def _parse_outstruct_args(outstruct, *args):
         if arg in outstruct_fieldnames:
             args_out.append(outstruct.get_field(arg))
         else:
-            args_out.append(arg)
+            if isinstance(arg, list):
+                if not allow_different_lengths and len(arg) != len(outstruct):
+                    raise ValueError(f"The list argument supplied is length {len(arg)}, which is not"
+                                     " the same as the provided OutStruct, which is length {len(outstruct)}.")
+                args_out.append(arg)
+            elif isinstance(arg, tuple):
+                args_out.append(arg)
+            else:
+                args_out.append([arg for _ in range(len(outstruct))])
     if len(args_out) > 1:
         return tuple(args_out)
     return args_out[0]
+                            
+def _all_equal_list(iterable):
+    g = groupby(iterable)
+    return next(g, True) and not next(g, False)
 
