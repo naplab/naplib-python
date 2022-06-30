c97d6c1d6925df28084a30726e4d45a5ec256091
Gavin Mischler <gavinmischler@gmail.com>
2022-06-29 23:13:37 -0400

    Update operations.py (#37)
    * Update operations.py
    
    * Update operations.py

--- naplib/array_ops/operations.py
+++ naplib/array_ops/operations.py
@@ -126,6 +126,10 @@ def concat_apply(data_list, function, axis=0, function_kwargs=None):
         List of arrays after chopping up the output of the function into arrays
         of the same length as the original input.
     
+    Raises
+    ------
+    RuntimeError: If the callable function changes the size of the concatenation/splitting axis.
+    
     Examples
     --------
     >>> import numpy as np
@@ -178,12 +182,17 @@ def concat_apply(data_list, function, axis=0, function_kwargs=None):
     lengths = np.array([x.shape[axis] for x in data_list])
     data_cat = np.concatenate(data_list, axis=axis)
     
+    N = data_cat.shape[axis]
+    
     if function_kwargs is None:
         function_kwargs = {}
     if not isinstance(function_kwargs, dict):
         raise TypeError(f'function_kwargs must be a dict of keyword arguments, but got {type(function_kwargs)}')
 
     func_output = function(data_cat, **function_kwargs)
+    
+    if func_output.shape[axis] != N:
+        raise RuntimeError(f'The callable function changed the size of the concatenation and splitting axis from {N} to {func_output.shape[axis]}, but this dimension must remain constant.')
 
     # split output back into list, but cut off the last because it is an empty array
     output = [x for x in np.split(func_output, np.cumsum(lengths), axis=axis)[:-1]]
