33c54ffe5ad079288c5d3dba2def923bc2c00f16
Gavin Mischler <gavinmischler@gmail.com>
2022-08-16 14:55:55 -0400

    features module now includes all alignment functions

--- naplib/alignment/__init__.py
+++
@@ -1,6 +0,0 @@
-from .aligner import Aligner
-from .alignment import get_phoneme_label_vector
-from .alignment import get_word_label_vector
-from .alignment import create_wrd_dict
-
-__all__  = ['Aligner','get_phoneme_label_vector','get_word_label_vector','create_wrd_dict']
