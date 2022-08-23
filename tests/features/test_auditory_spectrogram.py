0076039ca2f8b1c0f7b1591c1c9888e134c0ce8a
Gavin Mischler <gavinmischler@gmail.com>
2022-08-22 22:36:29 -0400

    indents

--- tests/features/test_auditory_spectrogram.py
+++ tests/features/test_auditory_spectrogram.py
@@ -5,13 +5,13 @@ from naplib.features import auditory_spectrogram
 
 @pytest.fixture(scope='module')
 def small_sound():
-	fs = 10000
-	t = 5
-	f = 800
+    fs = 10000
+    t = 5
+    f = 800
     samples = np.linspace(0, t, int(fs*t), endpoint=False)
     signal = np.sin(2 * np.pi * f * samples)
     return data
 
 def test_small_sound_linear_factor(small_sound):
-	spec = auditory_spectrogram(small_sound, 10000, frame_len=8, tc=4, factor='linear')
-	assert spec.shape[1] == 128
+    spec = auditory_spectrogram(small_sound, 10000, frame_len=8, tc=4, factor='linear')
+    assert spec.shape[1] == 128
