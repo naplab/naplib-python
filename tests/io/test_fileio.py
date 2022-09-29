4f03c4f0eb01023dae01bf5b999418ae5199f799
Gavin Mischler <gavinmischler@gmail.com>
2022-09-29 19:50:14 -0400

    tests for import_outstruct' (#76)

--- tests/io/test_fileio.py
+++ tests/io/test_fileio.py
@@ -18,8 +18,6 @@ def data():
                  })
 
 
-# import_outstruct tests
-
 def test_save_dict():
     data = {'x': np.array([1,2,3]), 'string': 'message'}
     save('saved_test_data.pkl', data)
@@ -35,6 +33,26 @@ def test_load_dict():
 def test_load_no_file():
     with pytest.raises(FileNotFoundError):
         load('no_file_with_this_name.pkl')
+
+
+# import_outstruct tests
+
+def test_import_Data_works():
+    thisfile = os.path.dirname(__file__)
+    data = import_outstruct(f'{thisfile}/../../naplib/io/sample_data/demo_data.mat')
+    assert isinstance(data, Data)
+
+def test_import_Data_fields():
+    thisfile = os.path.dirname(__file__)
+    data = import_outstruct(f'{thisfile}/../../naplib/io/sample_data/demo_data.mat')
+    assert all(x==y for x, y in zip(data.fields, ['name','sound','soundf','dataf','duration','befaft','resp','aud','script']))
+    assert data[1]['name'] == 'stim02'
+    assert data[1]['resp'].shape == (5203, 10)
+    assert data[1]['aud'].shape == (5203, 128)
+    assert np.allclose(data[1]['resp'][:5,2], np.array([0.17288463, 0.27798367, 0.42636263, 0.46837241, 0.42326083]))
+    assert data[0]['script'][:5] == 'IT IS'
+    assert np.allclose(data[-1]['befaft'], np.array([1, 1], dtype='uint8'))
+    assert np.allclose(data['sound'][0][20000:20005], np.array([-0.04907227, -0.04403687, -0.03979492, -0.03573608, -0.03210449]))
 
 # export_outstruct tests
 
