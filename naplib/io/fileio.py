a1e680b122a74203bce53f72a6e75fef17744d39
Gavin Mischler <gavinmischler@gmail.com>
2022-10-03 02:19:24 -0400

    updates merge

--- naplib/io/fileio.py
+++ naplib/io/fileio.py
@@ -3,28 +3,34 @@ import warnings
 import numpy as np
 from tqdm import tqdm
 from hdf5storage import loadmat, savemat
+import h5py
 
 from ..data import Data
 
 ACCEPTED_CROP_BY = ['onset', 'durations']
 
-def import_data(filepath, strict=True):
+def import_data(filepath, strict=True, useloadmat=True, verbose=False):
     '''
     Import Data object from MATLAB (.mat) format. This will
     automatically transpose the 'resp' and 'aud' fields
     so that they are shape (time, channels) for each trial. The
-    Data object in MATLAB is a 1xN-shaped struct containing N trials
-    and any number of fields for each trial.
+    MATLAB equivalent structure is a 1xN struct with N trials and
+    some number of fields, and this is stored in the .mat file
+    under the variable name "out".
 
     Parameters
     ----------
     filepath : string
-        Path to .mat file with Data object.
+        Path to .mat file.
     strict : bool, default=True
         If True, requires strict adherance to the following standards:
         1) Each trial must contain at least the following fields:
         ['name','sound','soundf','resp','dataf']
         2) Each trial must contain the exact same set of fields
+    useloadmat : boolean, default=True
+        If True, use hdf5storage.loadmat, else use custom h5py loader
+    verbose : boolean, default=False
+        If True, print trial number and field as it's loaded
 
     Returns
     -------
@@ -32,48 +38,87 @@ def import_data(filepath, strict=True):
     
     Notes
     -----
-    Given the highly-specific nature of the Matlab Data format, this
+    Given the highly-specific nature of the Data object Matlab format, this
     function is mostly used internally by Neural Acoustic Processing
     Lab members.
     '''
-    loaded = loadmat(filepath)
-    loaded = loaded['out'].squeeze()
-    fieldnames = loaded[0].dtype.names
-    
     req = ['name','sound','soundf','resp','dataf']
-    
-    
     data = []
-    for trial in loaded:
-        trial_dict = {}
-        for f, t in zip(fieldnames, trial):
-            tmp_t = t.squeeze()
-            if f == 'resp' or f == 'aud':
-                if tmp_t.ndim > 1:
-                    tmp_t = tmp_t.transpose(1,0,*[i for i in range(2, tmp_t.ndim)]) # only switch the first 2 dimensions if there are more than 2
-            try:
-                tmp_t = tmp_t.item()
-            except:
-                pass
-            trial_dict[f] = tmp_t
-        data.append(trial_dict)
+    if useloadmat:
+        loaded = loadmat(filepath)
+        loaded = loaded['out'].squeeze()
+        fieldnames = loaded[0].dtype.names
+
+        for tt,trial in enumerate(loaded):
+            trial_dict = {}
+            for f, t in zip(fieldnames, trial):
+                if verbose: print(tt, f)
+                tmp_t = t.squeeze()
+                if f == 'resp' or f == 'aud':
+                    if tmp_t.ndim > 1:
+                        tmp_t = tmp_t.transpose(1,0,*[i for i in range(2, tmp_t.ndim)]) # only switch the first 2 dimensions if there are more than 2
+                try:
+                    tmp_t = tmp_t.item()
+                except:
+                    pass
+                trial_dict[f] = tmp_t
+            data.append(trial_dict)
+    else:
+        f = h5py.File(filepath)
+        fieldnames = list(f['out'].keys())
+        n_trial = f['out'][fieldnames[0]].shape[0]
     
-    fieldnames = set(data[0].keys())
+        for trial in range(n_trial):
+            trial_dict = {}
+            for fld in fieldnames:
+                if verbose: print(trial, fld)
+                tmp = np.array(f[f['out'][fld][trial][0]])
+                # Pull out scalars
+                if np.prod(tmp.shape) == 1:
+                    tmp = tmp[0,0]
+                else:
+                    try:
+                        tmp = ''.join([chr(c[0]) for c in tmp])
+                    except:
+                        # Read cell arrays within entries
+                        if isinstance(tmp[0,0], h5py.h5r.Reference):
+                            shp = tmp.shape
+                            tmp_flat = np.ravel(tmp)
+                            for tt in range(len(tmp_flat)):
+                                # Handle cell arrays containing strings
+                                try:
+                                    tmp_flat[tt] = ''.join([chr(c[0]) for c in f[tmp_flat[tt]][:]])
+                                except:
+                                    tmp_flat[tt] = f[tmp_flat[tt]][:]
+                            tmp = np.reshape(tmp_flat, shp)
+                            # Remove lists with single item
+                            try:
+                                while len(tmp) == 1:
+                                    tmp = tmp[0]
+                            except:
+                                pass
+                        tmp = np.squeeze(tmp)
+
+                trial_dict[fld] = tmp
+            data.append(trial_dict)
+    
     for r in req:
         if strict and r not in fieldnames:
             raise ValueError(f'Missing required field: {r}')
     
-    data = Data(data=data, strict=strict)
-    return data
+    out = Data(data=data, strict=strict)
+    return out
 
 def export_data(filepath, data, fmt='7.3'):
     '''
     Export a naplib.Data instance to the MATLAB-compatible
-    equivalent (.mat file). This will
+    equivalent (.mat file).
+    The MATLAB equivalent structure is a 1xN struct with N trials and
+    some number of fields, and this is stored in the .mat file
+    under the variable name "out". This function will
     automatically transpose the 'resp' and 'aud' fields for
     each trial in the .mat file, thus undoing the actions of
-    import_data. The Data object in MATLAB is a 1xN-shaped
-    struct containing N trials and any number of fields for each trial.
+    import_data.
 
     Parameters
     ----------
@@ -376,4 +421,3 @@ def _crop_raw_bids(raw_instance, crop_by, befaft):
         raws.append(raw_crop)
     
     return raws
-    
--- tests/io/test_fileio.py
+++ tests/io/test_fileio.py
@@ -3,6 +3,7 @@ import pytest
 import numpy as np
 
 from naplib.io import save, load, import_data, export_data
+
 from naplib import Data
 
 @pytest.fixture(scope='module')
@@ -38,24 +39,40 @@ def test_load_no_file():
 
 def test_import_Data_works():
     thisfile = os.path.dirname(__file__)
-    data = import_data(f'{thisfile}/../../naplib/io/sample_data/demo_data.mat')
+    fn = f'{thisfile}/../../naplib/io/sample_data/demo_data.mat'
+    data = import_data(fn)
+    assert isinstance(data, Data)
+    data = import_data(fn, useloadmat=False)
     assert isinstance(data, Data)
 
 def test_import_Data_fields():
     thisfile = os.path.dirname(__file__)
-    data = import_data(f'{thisfile}/../../naplib/io/sample_data/demo_data.mat')
-    assert all(x==y for x, y in zip(data.fields, ['name','sound','soundf','dataf','duration','befaft','resp','aud','script']))
+    fn = f'{thisfile}/../../naplib/io/sample_data/demo_data.mat'
+    data = import_data(fn)
+    assert all(x==y for x, y in zip(data.fields, ['name','sound','soundf','dataf','duration','befaft','resp','aud','script','chname']))
     assert data[1]['name'] == 'stim02'
     assert data[1]['resp'].shape == (5203, 10)
     assert data[1]['aud'].shape == (5203, 128)
     assert np.allclose(data[1]['resp'][:5,2], np.array([0.17288463, 0.27798367, 0.42636263, 0.46837241, 0.42326083]))
     assert data[0]['script'][:5] == 'IT IS'
+    assert data[0]['chname'][2] == 'Fz'
+    assert np.allclose(data[-1]['befaft'], np.array([1, 1], dtype='uint8'))
+    assert np.allclose(data['sound'][0][20000:20005], np.array([-0.04907227, -0.04403687, -0.03979492, -0.03573608, -0.03210449]))
+
+    data = import_data(fn, useloadmat=False)
+    assert set(data.fields)==set(['name','sound','soundf','dataf','duration','befaft','resp','aud','script','chname'])
+    assert data[1]['name'] == 'stim02'
+    assert data[1]['resp'].shape == (5203, 10)
+    assert data[1]['aud'].shape == (5203, 128)
+    assert np.allclose(data[1]['resp'][:5,2], np.array([0.17288463, 0.27798367, 0.42636263, 0.46837241, 0.42326083]))
+    assert data[0]['script'][:5] == 'IT IS'
+    assert data[0]['chname'][2] == 'Fz'
     assert np.allclose(data[-1]['befaft'], np.array([1, 1], dtype='uint8'))
     assert np.allclose(data['sound'][0][20000:20005], np.array([-0.04907227, -0.04403687, -0.03979492, -0.03573608, -0.03210449]))
 
 # export_data tests
 
-def test_export_bad_data_object(data):
+def test_export_bad_format(data):
     with pytest.raises(TypeError):
         export_data('fname_out_test.mat', {'bad': 'data'})
 
@@ -66,5 +83,3 @@ def test_export_bad_format(data):
 def test_export_Data(data):
     export_data('fname_out_test.mat', data)
     pass
-
-
