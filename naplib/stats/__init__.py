865374dbc3fc2affa7563c62d53e37ad91ba8f39
Gavin Mischler <gavinmischler@gmail.com>
2022-10-30 16:38:37 -0400

    add ttest

--- naplib/stats/__init__.py
+++ naplib/stats/__init__.py
@@ -2,6 +2,7 @@ from .encoding import dprime
 from .encoding import fratio
 from .mixedeffectsmodel import LinearMixedEffectsModel
 from .pvalues import stars
-from .responsive_elecs import responsive_ttest
+from .responsive_ttest import responsive_ttest
+from .ttest import ttest
 
-__all__  = ['dprime','fratio','LinearMixedEffectsModel','stars','responsive_ttest']
+__all__  = ['dprime','fratio','LinearMixedEffectsModel','stars','responsive_ttest', 'ttest']
