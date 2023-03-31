02599d82858f6d9a51c0f179538a5791efd7d49f
Menoua Keshishian <39179450+menoua@users.noreply.github.com>
2023-03-31 18:16:20 -0400

    Update __init__.py

--- naplib/io/__init__.py
+++ naplib/io/__init__.py
@@ -3,5 +3,5 @@ from .load_sample import load_speech_task_data
 from .read_htk import read_htk
 from .load_edf import load_edf
 
-__all__ = ['load','save','import_data','export_data','read_bids','load_speech_task_data','read_htk']
+__all__ = ['load','save','import_data','export_data','read_bids','load_speech_task_data','read_htk','load_edf']
 
