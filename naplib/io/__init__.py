f0e212c66525232dadd6b097b3b9c303c988d1f2
Menoua Keshishian <39179450+menoua@users.noreply.github.com>
2023-03-31 18:14:20 -0400

    Load EDF functionality

--- naplib/io/__init__.py
+++ naplib/io/__init__.py
@@ -1,6 +1,7 @@
 from .fileio import load, save, import_data, read_bids, export_data
 from .load_sample import load_speech_task_data
 from .read_htk import read_htk
+from .load_edf import load_edf
 
 __all__ = ['load','save','import_data','export_data','read_bids','load_speech_task_data','read_htk']
 
