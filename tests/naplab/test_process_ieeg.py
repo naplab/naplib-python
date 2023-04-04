46ef432c7799ef41398af1eed63e1df859a92713
Gavin Mischler <gavinmischler@gmail.com>
2023-04-04 17:09:06 -0400

    test for pca

--- tests/naplab/test_process_ieeg.py
+++ tests/naplab/test_process_ieeg.py
@@ -14,6 +14,12 @@ def small_data():
     d = load(os.path.join(dir_path, 'data_small.pkl'))
     return {'path': dir_path, 'data_dict': d}
 
+@pytest.fixture(scope='module')
+def small_data_fs50():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    dir_path = os.path.join(dir_path, 'test_processing_pipeline_data_small_50fs')
+    d = load(os.path.join(dir_path, 'data_small_fs50.pkl'))
+    return {'path': dir_path, 'data_dict': d}
 
 def test_single_stimuli_pipeline(small_data):
 
@@ -64,6 +70,7 @@ def test_single_stimuli_pipeline(small_data):
 
     # check spectrograms correct shape
     assert data_out['aud'][0].shape == (1200,128)
+    assert np.allclose(data_out['aud'][0][10,:], np.zeros((128,)))
 
 
 def test_single_stimuli_spectrum_inference_method(small_data):
@@ -152,17 +159,17 @@ def test_single_stimuli_pipeline_with_rereference(small_data):
     assert np.allclose(data_out[0]['reference'][:,0], true_data['data'][1900:3100].mean(1))
 
 
-def test_single_stimuli_pipeline_with_rereference_downsample(small_data):
-
-    dir_path = small_data['path']
-    true_data = small_data['data_dict']
+def test_single_stimuli_pipeline_with_rereference_downsample(small_data_fs50):
+    """This data has a sampling rate of 50, which is different from wav file stimulus"""
+    dir_path = small_data_fs50['path']
+    true_data = small_data_fs50['data_dict']
 
     data_out = process_ieeg(
         dir_path,
         dir_path,
         rereference_grid=np.ones((2,2)),
         rereference_method='pca',
-        intermediate_fs = 80,
+        intermediate_fs = 40,
         final_fs = 10,
         bands=['raw'],
         store_reference=True,
@@ -181,7 +188,6 @@ def test_single_stimuli_pipeline_with_rereference_downsample(small_data):
     assert data_out['raw'][0].shape == (120,2) # 2 seconds from befaft, plus 10 seconds of trial at 100 Hz
 
     # check reference is nan (since two channels are identical)
-    assert np.all(np.isnan(data_out['reference'][0]))
     assert data_out['reference'][0].shape == (120,2)
 
     # shape of output correct
