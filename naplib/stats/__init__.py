7c12e8a1e869eb580485aba752cdfbea87f5ec2a
Gavin Mischler <gavinmischler@gmail.com>
2023-05-08 15:55:00 -0400

    wilks lambda discriminability (#169)
    * wilks lambda fratio
    
    * gitignore
    
    * tests
    
    * update version to 0.5.0
    
    * Add `pyproject.toml` to `.gitignore`
    
    ---------
    
    Co-authored-by: Menoua Keshishian <mk4011@columbia.edu>

--- naplib/stats/__init__.py
+++ naplib/stats/__init__.py
@@ -1,8 +1,7 @@
-from .encoding import dprime
-from .encoding import fratio
+from .encoding import discriminability
 from .mixedeffectsmodel import LinearMixedEffectsModel
 from .pvalues import stars
 from .responsive_ttest import responsive_ttest
 from .ttest import ttest
 
-__all__  = ['dprime','fratio','LinearMixedEffectsModel','stars','responsive_ttest', 'ttest']
+__all__  = ['discriminability','LinearMixedEffectsModel','stars','responsive_ttest', 'ttest']
