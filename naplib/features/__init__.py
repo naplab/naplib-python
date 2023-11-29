fdca263e646dd60c22706993411787df887e81a5
Vinay Raghavan <42253618+vinaysraghavan@users.noreply.github.com>
2023-11-29 15:43:45 -0500

    Camel case in __init__

--- naplib/features/__init__.py
+++ naplib/features/__init__.py
@@ -1,6 +1,6 @@
 from .aligner import Aligner
 from .alignment_extras import get_phoneme_label_vector, get_word_label_vector, create_wrd_dict
 from .auditory_spectrogram import auditory_spectrogram
-from .peakRate import peakRate
+from .peak_rate import peak_rate
 
-__all__  = ['Aligner','get_phoneme_label_vector','get_word_label_vector','create_wrd_dict','auditory_spectrogram','peakRate']
+__all__  = ['Aligner','get_phoneme_label_vector','get_word_label_vector','create_wrd_dict','auditory_spectrogram','peak_rate']
