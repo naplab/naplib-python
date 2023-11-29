f522e59c4cd9f5e15e80b5efb46982947fdc1ed0
Vinay Raghavan <42253618+vinaysraghavan@users.noreply.github.com>
2023-11-29 15:41:22 -0500

    Snake case
    Changing all camel case to snake case, fixing doc string for citation

--- naplib/features/peak_rate.py
+++ naplib/features/peak_rate.py
@@ -3,9 +3,9 @@ from scipy.signal import butter, filtfilt, find_peaks
 from scipy.stats import zscore
 
 
-def peakRate(aud, sfreq, band=[1,10], th=0):
+def peak_rate(aud, sfreq, band=[1,10], thresh=0):
     '''
-    Extract peakRate events from the (auditory) spectrogram
+    Extract peak_rate events from the (auditory) spectrogram
     
     Parameters
     ----------
@@ -15,16 +15,19 @@ def peakRate(aud, sfreq, band=[1,10], th=0):
         Sampling rate of the spectrogram
     band : List, default=[1,10]
         Envelope bandpass filter bandwidth
-    th: float, default=0
-        Threshold (in units of standard deviations) of peaks to keep
+    thresh: float, default=0
+        Threshold (in units of standard deviations) of peaks to retain
 
     Returns
     -------
-    peakRate : np.ndarray
-        Time-series of peakRate events, shape (n_frames, )
+    peak_rate : np.ndarray
+        Time-series of peak_rate events, shape (n_frames, )
 
-    See Oganian, Y., & Chang, E. F. (2019). A speech envelope landmark for
-    syllable encoding in human superior temporal gyrus. Science advances
+    References
+    ----------
+    [#1oganian] Y. Oganian, & E.F. Chang, "A speech envelope landmark for
+    syllable encoding in human superior temporal gyrus," in Science Advances,
+    vol 5, no. 11, pp. eaay6279, November 2019, doi: 10.1126/sciadv.aay6279
     '''
     
     # Extract the envelope by taking the mean over frequencies
@@ -38,8 +41,8 @@ def peakRate(aud, sfreq, band=[1,10], th=0):
     rate = np.maximum(np.diff(env, prepend=env[0]), 0)
 
     # Extract local peaks
-    peakRate = np.zeros_like(env)
-    peaks = find_peaks(rate, height=th*np.std(rate))[0]
-    peakRate[peaks] = rate[peaks]
+    peak_rate = np.zeros_like(env)
+    peaks = find_peaks(rate, height=thresh*np.std(rate))[0]
+    peak_rate[peaks] = rate[peaks]
 
-    return peakRate
+    return peak_rate
