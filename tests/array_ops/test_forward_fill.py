c116589cd2c1ad611bf8a3f0496e316a39704b75
Gavin Mischler <gavinmischler@gmail.com>
2024-01-21 22:36:52 -0500

    Update test_forward_fill.py

--- tests/array_ops/test_forward_fill.py
+++ tests/array_ops/test_forward_fill.py
@@ -14,7 +14,7 @@ def test_forward_fill_axis0():
                        [ 2.,  1.,  3., np.nan],
                        [ 2.,  1.,  3., np.nan]])
   output = forward_fill(arr, axis=0)
-  assert np.allclose(output, expected)
+  assert np.allclose(output, expected, equal_nan=True)
 
 def test_forward_fill_axis1():
   arr = np.nan*np.ones((5,4))
@@ -27,15 +27,15 @@ def test_forward_fill_axis1():
                        [np.nan, np.nan, np.nan, np.nan],
                        [np.nan, np.nan, np.nan, np.nan]])
   output = forward_fill(arr, axis=1)
-  assert np.allclose(output, expected)
+  assert np.allclose(output, expected, equal_nan=True)
 
 def test_forward_fill_1d():
   arr = np.nan*np.ones((6,))
   arr[1] = 1
-  arr[3,0] = 2
+  arr[3] = 2
   expected = np.array([np.nan,1,1,2,2,2])
   output = forward_fill(arr, axis=0)
-  assert np.allclose(output, expected)
+  assert np.allclose(output, expected, equal_nan=True)
 
 def test_bad_input():
   arr = np.nan*np.ones((5,4))
