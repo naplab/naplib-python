94afd1579e589336a9ad279c3f22fe431bac2340
Gavin Mischler <gavinmischler@gmail.com>
2025-03-01 14:31:29 -0500

    Update localization __init__

--- naplib/localization/__init__.py
+++ naplib/localization/__init__.py
@@ -1,3 +1,4 @@
 from .freesurfer import Brain, find_closest_vertices
+from .coordinate_conversions import mni152_to_fsaverage, fsaverage_to_mni152
 
-__all__ = ['Brain', 'find_closest_vertices']
+__all__ = ['Brain', 'find_closest_vertices', 'mni152_to_fsaverage', 'fsaverage_to_mni152']
