a948801b30686ceaa7f09fb779a47009ab7da998
Arsalan Firoozi <personalaccount@Arsalans-MacBook-Pro-6.local>
2025-07-04 11:27:03 -0400

    General Conversion: supporting both hemispheres

--- naplib/localization/coordinate_conversions.py
+++ naplib/localization/coordinate_conversions.py
@@ -44,12 +44,12 @@ def src_to_dst(coords, src_pial, src_sphere, dst_pial, dst_sphere):
     """
     Convert 3D coordinates from any space to another space.
     Each subject comes with a bunch of MRI files; In this function these files are used:
-    1. lh/rh.pial file of the source space      ==> SRC_PATH/surf/lh.pial
-    2. lh/rh.sphere.reg file of the source      ==> SRC_PATH/surf/lh.sphere.reg
-    3. lh/rh.pial file of the destination       ==> DST_PATH/surf/lh.pial
-    4. lh/rh.sphere.reg file of the destination ==> DST_PATH/surf/lh.sphere.reg
+    1. lh.pial file of the source space      ==> SRC_PATH/surf/lh.pial
+    2. lh.sphere.reg file of the source      ==> SRC_PATH/surf/lh.sphere.reg
+    3. lh.pial file of the destination       ==> DST_PATH/surf/lh.pial
+    4. lh.sphere.reg file of the destination ==> DST_PATH/surf/lh.sphere.reg
 
-    Apply this function separately for left and right hemispheres.
+    Provide LH files, the function assumes the RH ones are in the same directory.
 
     NOTE: In case of converting to an atlas space, the files we need are accessible
     by installing freesurfer: https://surfer.nmr.mgh.harvard.edu/fswiki/DownloadAndInstall
@@ -58,9 +58,10 @@ def src_to_dst(coords, src_pial, src_sphere, dst_pial, dst_sphere):
     Parameters
     ----------
     coords : np.ndarray (elecs, 3)
-        Coordinates in source space
-    src_pial : str
-        Path to the source pial surface file (e.g., 'lh.pial')
+        Coordinates in source space. Can be in both hemispheres.
+    src_pial : str/dict{'vert_lh', 'vert_rh'}
+        Path to the source pial surface file (e.g., 'lh.pial'). In case of a mat file for pial surfaces,
+        provide a dictionary with keys 'vert_lh' and 'vert_rh' containing the vertices for each hemisphere.
     src_sphere : str
         Path to the source sphere file (e.g., 'lh.sphere.reg')
     dst_pial : str
@@ -73,23 +74,49 @@ def src_to_dst(coords, src_pial, src_sphere, dst_pial, dst_sphere):
     new_coords : np.ndarray (elecs, 3)
         Coordinates in target space
     """
-    src_sphere, _ = fsio.read_geometry(src_sphere)
-    tgt_sphere, _ = fsio.read_geometry(dst_sphere)
+    src_sphere_lh, _ = fsio.read_geometry(src_sphere)
+    src_sphere_rh, _ = fsio.read_geometry(src_sphere.replace('lh', 'rh'))
+    src_sphere = np.vstack((src_sphere_lh, src_sphere_rh))
 
-    tree = cKDTree(tgt_sphere)
+    tgt_sphere_lh, _ = fsio.read_geometry(dst_sphere)
+    tgt_sphere_rh, _ = fsio.read_geometry(dst_sphere.replace('lh', 'rh'))
+
+    tree_lh = cKDTree(tgt_sphere_lh)
+    tree_rh = cKDTree(tgt_sphere_rh)
 
     if np.isnan(coords).any():
         print("WARNING: NaN values found in coordinates. Replacing with zeros.")
         coords = np.nan_to_num(coords)
 
-    lh_verts_sub, _ = fsio.read_geometry(src_pial)
+    if isinstance(src_pial, str):
+        lh_verts_sub, _ = fsio.read_geometry(src_pial)
+        rh_verts_sub = fsio.read_geometry(src_pial.replace('lh', 'rh'))[0]
+        lh_threshold = lh_verts_sub.shape[0]
+        lh_verts_sub = np.vstack((lh_verts_sub, rh_verts_sub))
+    else:
+        lh_verts_sub = src_pial['vert_lh']
+        rh_verts_sub = src_pial['vert_rh']
+        lh_threshold = lh_verts_sub.shape[0]
+
     lh_verts_sub_fs, _ = fsio.read_geometry(dst_pial)
-    
+    rh_verts_sub_fs, _ = fsio.read_geometry(dst_pial.replace('lh', 'rh'))
+
     tree_elecs = cKDTree(lh_verts_sub)
     _, mapping_indices_elecs = tree_elecs.query(coords, k=1)
+    
+    print(f"#Electrodes in LH: {np.sum(mapping_indices_elecs < lh_threshold)}, RH: {np.sum(mapping_indices_elecs >= lh_threshold)}")
 
-    _, mapping_indices_elecs_warped = tree.query(src_sphere[mapping_indices_elecs], k=1)
+    mapping_indices_elecs_lh = mapping_indices_elecs[mapping_indices_elecs < lh_threshold]
+    _, mapping_indices_elecs_warped_lh = tree_lh.query(src_sphere[mapping_indices_elecs_lh], k=1)
 
-    new_coords = lh_verts_sub_fs[mapping_indices_elecs_warped]
+    mapping_indices_elecs_rh = mapping_indices_elecs[mapping_indices_elecs >= lh_threshold]
+    _, mapping_indices_elecs_warped_rh = tree_rh.query(src_sphere[mapping_indices_elecs_rh - lh_threshold], k=1)
+
+    new_coords_lh = lh_verts_sub_fs[mapping_indices_elecs_warped_lh]
+    new_coords_rh = rh_verts_sub_fs[mapping_indices_elecs_warped_rh]
+
+    new_coords = np.zeros((coords.shape[0], 3))
+    new_coords[mapping_indices_elecs < lh_threshold] = new_coords_lh
+    new_coords[mapping_indices_elecs >= lh_threshold] = new_coords_rh
 
     return new_coords
